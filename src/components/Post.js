f6259096c5e8a76c0378b8e265386a39d6ef2a9c
dilshanhiruna <dilshan.hirunaa@gmail.com>
2021-06-22 19:48:03 +0530

    Implemented Redux

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
 
-export default class   extends Component {
+class Post extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            post: []
+        }
+    }
+    componentWillUnmount() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(data => this.setState({ post: data }));
+    }
+
     render() {
+        const postItems = this.state.post.map(post => (
+            <div key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+            </div>
+        ));
         return (
             <div>
-                
+
+                <p>POST</p>
+                {postItems}
             </div>
         )
     }
 }
+
+export default Post;
